da8fe18291c4deca53581efd273aa82526beb632
pavloleskovych <pleskovych@gmail.com>
2026-02-05 16:55:21 +0100

    Refactor API routes for cryptocurrencies
    - Updated route prefix from "/currencies" to "/cryptocurrencies"
    - Simplified endpoint for fetching all cryptocurrencies to the root path
    - Adjusted dynamic currency ID endpoint to use a more concise path

--- backend/router.py
+++ backend/router.py
@@ -3,15 +3,15 @@ from backend.init import cmc_client
 
 
 router_crypto = APIRouter(
-    prefix="/currencies"
+    prefix="/cryptocurrencies"
 )
 
 
-@router_crypto.get("/cryptocurrencies")
+@router_crypto.get("")
 async def get_cryptocurrencies():
     return await cmc_client.get_listings()
 
 
-@router_crypto.get("/cryptocurrencies/{currency_id}")
+@router_crypto.get("/{currency_id}")
 async def get_cryptocurrency(currency_id: int):
     return await cmc_client.get_currency(currency_id=currency_id)
